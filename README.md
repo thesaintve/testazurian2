1ccaa5a73c967de989aea11ecaa924429f5d6ba6
David Besson <david.besson@gmail.com>
2019-01-07 15:58:25 -0500

    Para ejecucion local con MySQL

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Instrucciones
+
+-- 1) Crear la base de datos dbprueba
+-- 2) Crear la tabla vehiculo
+-- 3) Configurar la cadena de conexion, usuario y clave del persistence.xml
+-- 4) Abrir el proyecto en esta ruta http://localhost:8080/SpringAngular03-1.0
+-- 5) El endpint Rest http://localhost:8080/SpringAngular03-1.0/api/vehiculo
+
---
+++ target/generated-sources/annotations/com/mycom/modelo/Vehiculo_.java
@@ -0,0 +1,17 @@
+package com.mycom.modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-07T15:34:24")
+@StaticMetamodel(Vehiculo.class)
+public class Vehiculo_ { 
+
+    public static volatile SingularAttribute<Vehiculo, Integer> kilometrajeIni;
+    public static volatile SingularAttribute<Vehiculo, String> alta;
+    public static volatile SingularAttribute<Vehiculo, Integer> kilometraje;
+    public static volatile SingularAttribute<Vehiculo, String> modelo;
+    public static volatile SingularAttribute<Vehiculo, String> matricula;
+
+}
